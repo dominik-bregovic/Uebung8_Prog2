6e53d3db84e3f58e5f0587a85e22b49e8f7fc967
dominik-github1 <80465736+dominik-github1@users.noreply.github.com>
2021-05-24 20:54:12 +0200

    added button

--- src/Users.java
+++ src/Users.java
@@ -1,11 +1,21 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Users {
+public class Users extends JFrame{
     private JPanel UserInterface;
     private JButton createUserButton;
     private JTextField textField1;
     private JTextField textField2;
     private JPasswordField passwordField1;
-    private JTextPane textPane1;
-    private JList list1;
+    private JTextField textField3;
+
+    public Users() {
+        createUserButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
